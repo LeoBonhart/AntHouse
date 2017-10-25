ba6e35b30e2575fdcc3350ff450d05c911bced7e
Валерий <faceless@static-void.ru>
2017-10-26 02:04:12 +0300

    add hotkey

--- src/app/content/home/review/send/send.component.ts
+++ src/app/content/home/review/send/send.component.ts
@@ -4,12 +4,27 @@ import { Review, ReviewService } from '../../../../shared/index';
 @Component({
     selector: 'send',
     templateUrl: './send.component.html',
+    host: {'(window:keydown)': 'hotkeys($event)',
+           '(window:keyup)': 'clearkeys()'},
     styleUrls: ['./send.component.scss']
 })
 export class SendComponent implements OnInit {
     model: Array<Review>;
     text: string = '';
+    keys: Array<number>;
+    private conkeys: Array<number> = [17, 13];
 
+    hotkeys(event: any) {
+        this.keys.push(event.keyCode);
+        if (this.conkeys.indexOf(this.keys[0]) >= 0 && this.conkeys.indexOf(this.keys[1]) >= 0) {
+           this.addMessage();
+           this.conkeys = [];
+        }
+    }
+    clearkeys() {
+        this.keys = [];
+        this.conkeys = [17, 13];
+    }
     ngOnInit() {
     }
 
